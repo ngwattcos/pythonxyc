03957cff435ff4567bc08349ecbbbc39824e8cfe
Scott Wang <curiosity142000@gmail.com>
2020-10-27 18:32:58 -0400

    finalized tokens

--- lexer.mli
+++ lexer.mli
@@ -14,6 +14,7 @@ let _doublequote_ = '"'
 
 let _singlequote_ = '\''
 
+(* primitives *)
 let _true_ = 'True'
 let _false = 'False'
 
@@ -22,18 +23,28 @@ let _string_ = '"'[_^'"']'"'
 let _number_ = ['-']{0-1}['1'-'9']*['0'-'9']{1}['.']*['0'-'9']*
 let _var_ = ['a'-'z' 'A'-'Z' '_']+['a'-'z' 'A'-'Z' '_' '0'-'9']*
 
+(* assignment operators *)
 let _equals_ = '='
+let _plusequals_ = '+='
+let _minusequals_ = '-='
+let _timesequals_ = '*='
+let _divideequals_ = '/='
+let _moduloequals_ = '%='
+
+
+(* equality check *)
 let _doubleequals_ = '=='
 
-
+(* boolean logic operators *)
 let _and_ = 'and'
 let _or_ = 'or'
 let _not_ = 'not'
 
-
+(* object checks *)
 let _in_ = 'in'
 let _is_ = 'is'
 
+(* numerical operators *)
 let _plus_ = '+'
 let _minus_ = '-'
 let _times_ = '*'
@@ -46,6 +57,7 @@ let _ge_ = '>='
 let _lt_ = '<'
 let _le_ = '<='
 
+(* braces, brackets, and parentheses *)
 let _lparen- = '('
 let _rparen- = ')'
 
@@ -56,9 +68,12 @@ let _comma_ = ','
 let _lbrace_ = '{'
 let _rbrace_ = '{'
 
+(* block begin and end *)
 let _colon_ = ':'
 let _endblock_ = '@end'
+let _reactblock_ = '@react'
 
+(* control flow *)
 let _if_ = 'if'
 let _elif_ = 'elif'
 let _else_ = 'if'
@@ -73,6 +88,7 @@ let _continue_ = 'continue'
 
 let _class_ = 'class'
 
+(* functions *)
 let _def_ = 'def'
 let _lambda_ = 'lambda'
 
@@ -87,6 +103,19 @@ let _delete_ = 'del'
 
 
 
-(* rules *)
+(* 
 
-(* trailer *)
+rules
+
+ *)
+
+
+
+
+
+
+(* 
+
+trailer
+
+ *)

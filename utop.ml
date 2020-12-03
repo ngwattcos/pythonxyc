bf76e8fee48294250ef5e39af964248730363c5a
connielei <cl878@cornell.edu>
2020-12-03 14:22:26 -0500

    utop update

--- utop.ml
+++ utop.ml
@@ -8,3 +8,10 @@ let parse_program (filename:string) =
         (Lexing.lexeme_end lexbuf - !Lexer.linestart - 1);
       exit 1 in
   (ast)
+
+let name (num:int) = 
+  let numS = string_of_int num in
+  "tests/parser/parse" ^ 
+  (if num < 10 then "0" else "") ^ numS ^ ".pyx"
+
+let pnum (num:int) = parse_program (name num)

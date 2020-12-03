a81f275b3e7387b4e5e8da9b8087f74e997842e1
connielei <cl878@cornell.edu>
2020-12-03 10:44:22 -0500

    fill in aexp

--- transform.ml
+++ transform.ml
@@ -9,11 +9,11 @@ let indbuf = ref (Buffer.create 0)
 (* translation converts python-specific functions amd library calls *)
 (* to a javascript-friendly format *)
 
-let rec print_p (prog: Ast.program) = ()
+let rec print_p (prog: program) = ()
 
-let rec print_c (c: Ast.com) = ()
+let rec print_c (c: com) = ()
 
-let rec print_e (c: Ast.exp) = ()
+let rec print_e (c: exp) = ()
 
 (* let rec reverse (lst: 'a list) (acc: 'a list) = match lst with
 | h::tl -> reverse tl (h::acc)
@@ -21,25 +21,25 @@ let rec print_e (c: Ast.exp) = ()
 
 let reverse_list (lst: 'a list) = reverse lst [] *)
 
-(* let translatep (prog: Ast.program) = print_p prog; match prog with
+(* let translatep (prog: program) = print_p prog; match prog with
 | c::tl -> printf "a command"; ()
 | [] -> printf "end"; ()
 
-let rec translatec (c: Ast.com) = print_c c; match c with
+let rec translatec (c: com) = print_c c; match c with
 | _ -> () *)
 
 
 (* len(var) -> var.length (does not matter if string or arr) *)
 (* array slice: arr[0, 3] -> arr.slice(0, 3) *)
-let rec translate_e (e: Ast.exp) = print_e e; match e with
+let rec translate_e (e: exp) = print_e e; match e with
 | e -> e
 
 
-let rec transform_p (prog: Ast.program) (buf: Buffer.t) =
+let rec transform_p (prog: program) (buf: Buffer.t) =
 transform_coms prog buf;
 buf
 
-and transform_coms (prog: Ast.program) (buf: Buffer.t) = match prog with
+and transform_coms (prog: program) (buf: Buffer.t) = match prog with
 | [] -> ()
 | c::[] ->
     Buffer.add_buffer buf !indbuf;
@@ -51,7 +51,7 @@ and transform_coms (prog: Ast.program) (buf: Buffer.t) = match prog with
     ignore (transform_c c buf);
     Buffer.add_string buf "\n"
 
-and transform_c (c: Ast.com) (buf: Buffer.t) = match c with
+and transform_c (c: com) (buf: Buffer.t) = match c with
 | ValUpdate (val_update) ->
     transform_val_update val_update buf;
     Buffer.add_string buf ";"
@@ -91,7 +91,7 @@ and transform_c (c: Ast.com) (buf: Buffer.t) = match c with
     Buffer.add_string buf ";"
 | _ -> failwith "unimplemented command"
 
-and transform_val_update (c: Ast.val_update) (buf: Buffer.t) = match c with
+and transform_val_update (c: val_update) (buf: Buffer.t) = match c with
 | JLet (var, exp) ->
     Buffer.add_string buf "let ";
     transform_var var buf;
@@ -107,7 +107,7 @@ and transform_val_update (c: Ast.val_update) (buf: Buffer.t) = match c with
     transform_update_op update_op buf;
     transform_e exp buf
 
-and transform_update_op (op: Ast.update_op) (buf: Buffer.t) = match op with
+and transform_update_op (op: update_op) (buf: Buffer.t) = match op with
 | Equals ->
     Buffer.add_string buf " = "
 | PlusEquals ->
@@ -121,13 +121,13 @@ and transform_update_op (op: Ast.update_op) (buf: Buffer.t) = match op with
 | ModuloEquals ->
     Buffer.add_string buf " %= "
 
-and transform_e (exp: Ast.exp) (buf: Buffer.t) =
+and transform_e (exp: exp) (buf: Buffer.t) =
 let e = translate_e exp in match e with
 | Bexp (bexp) -> transform_bexp bexp buf
 | String (str) -> Buffer.add_string buf str
 | _ -> ()
 
-and transform_bexp (e: Ast.bexp) (buf: Buffer.t) =
+and transform_bexp (e: bexp) (buf: Buffer.t) =
 match e with
 | Or (b1, b2) ->
     transform_bexp b1 buf;
@@ -169,7 +169,7 @@ match e with
     transform_aexp a2 buf
 | Aexp (a) -> transform_aexp a buf
 
-and transform_aexp (e: Ast.aexp) (buf: Buffer.t) =
+and transform_aexp (e: aexp) (buf: Buffer.t) =
 match e with
 | Int (i) -> Buffer.add_string buf (string_of_int i)
 | Float (f) -> Buffer.add_string buf (string_of_float f)
@@ -179,25 +179,42 @@ match e with
     Buffer.add_string buf "(";
     transform_e e buf;
     Buffer.add_string buf ")"
-| Expon (a1, a2) -> failwith "unimplemented expon"
-| Times (a1, a2) -> failwith "unimplemented times"
-| Div (a1, a2) -> failwith "unimplemented divide"
-| Plus (a1, a2) -> failwith "unimplemented plus"
-| Minus (a1, a2) -> failwith "unimplemented minus"
-| Mod (a1, a2) -> failwith "unimplemented mod"
+| Expon (a1, a2) -> 
+    transform_aexp a1 buf;
+    Buffer.add_string buf " ** ";
+    transform_aexp a2 buf
+| Times (a1, a2) -> 
+    transform_aexp a1 buf;
+    Buffer.add_string buf " * ";
+    transform_aexp a2 buf
+| Div (a1, a2) -> 
+    transform_aexp a1 buf;
+    Buffer.add_string buf " / ";
+    transform_aexp a2 buf
+| Plus (a1, a2) ->
+    transform_aexp a1 buf;
+    Buffer.add_string buf " + ";
+    transform_aexp a2 buf
+| Minus (a1, a2) ->
+    transform_aexp a1 buf;
+    Buffer.add_string buf " - ";
+    transform_aexp a2 buf
+| Mod (a1, a2) ->
+    transform_aexp a1 buf;
+    Buffer.add_string buf " % ";
+    transform_aexp a2 buf
 
-
-and transform_func_call (fc: Ast.func_call) (buf: Buffer.t) = match fc with
+and transform_func_call (fc: func_call) (buf: Buffer.t) = match fc with
 | Call (var_access, args) ->
     transform_var_access var_access buf;
     Buffer.add_string buf "(";
     transform_args args buf;
     Buffer.add_string buf ")"
 
-and transform_var (v: Ast.var) (buf: Buffer.t) = Buffer.add_string buf v
+and transform_var (v: var) (buf: Buffer.t) = Buffer.add_string buf v
 
 (* Expands a var_access in  "reversed" (correct) order *)
-and transform_var_access (v: Ast.var_access) (buf: Buffer.t) = match v with
+and transform_var_access (v: var_access) (buf: Buffer.t) = match v with
 | Var (var) -> transform_var var buf
 | Dot (v1, var) ->
     transform_var_access v1 buf;
@@ -210,7 +227,7 @@ and transform_var_access (v: Ast.var_access) (buf: Buffer.t) = match v with
     Buffer.add_string buf "]"
 
 (* expands an argument list in "reversed" (correct) order *)
-and transform_args (lst: Ast.exp list) (buf: Buffer.t) = match lst with
+and transform_args (lst: exp list) (buf: Buffer.t) = match lst with
 | [] -> ()
 | arg::[] ->
     transform_e arg buf
@@ -220,7 +237,7 @@ and transform_args (lst: Ast.exp list) (buf: Buffer.t) = match lst with
     transform_e arg buf
 
 (* expands a parameter list in "reversed" (correct) order *)
-and transform_params (lst: Ast.var list) (buf: Buffer.t) = match lst with
+and transform_params (lst: var list) (buf: Buffer.t) = match lst with
 | [] -> ()
 | param::[] ->
     transform_var param buf
@@ -229,8 +246,6 @@ and transform_params (lst: Ast.var list) (buf: Buffer.t) = match lst with
     Buffer.add_string buf ", ";
     transform_var param buf
 
-
-
-let transform (prog: Ast.program) =
+let transform (prog: program) =
 ignore (indbuf :=  Buffer.create 0);
 transform_p prog (Buffer.create 0)

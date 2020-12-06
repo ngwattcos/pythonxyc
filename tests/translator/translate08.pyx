85d9f5adb0696d1005d3c6f3e5eb6f939b25e55a
Scott Wang <curiosity142000@gmail.com>
2020-12-06 16:52:39 -0500

    add tests for while, bexp

---
+++ tests/translator/translate08.pyx
@@ -0,0 +1,8 @@
+# bexp tests
+
+@let troo = True
+troo = true == True
+
+if troo != False and True or not troo == True:
+    print("something")
+@end
---
+++ tests/translator/translate09.pyx
@@ -0,0 +1,13 @@
+# while commands
+while t == True:
+    print("aijfa")
+    t = False
+    continue
+@end
+
+while i < len(arr):
+    print(arr[i])
+    print(len(arr))
+    i += 1
+@end
+

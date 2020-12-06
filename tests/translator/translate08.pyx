8c5368e54d4aee07408ccebee98034e30157a8fb
Scott Wang <curiosity142000@gmail.com>
2020-12-06 16:53:41 -0500

    update tests

--- tests/translator/translate08.pyx
+++ tests/translator/translate08.pyx
@@ -3,6 +3,6 @@
 @let troo = True
 troo = true == True
 
-if troo != False and True or not troo == True:
+if troo != False and True or not troo == True and (True or False and t + 1 > t):
     print("something")
 @end

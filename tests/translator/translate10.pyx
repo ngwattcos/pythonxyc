2e0bd207605f9554bb65cee0020d0afd00c2094c
Scott Wang <curiosity142000@gmail.com>
2020-12-06 18:23:50 -0500

    add for command tests

---
+++ tests/translator/translate10.pyx
@@ -0,0 +1,17 @@
+# for commands!
+
+for i in range(5):
+    continue
+@end
+
+for i in range(0, 5):
+    continue
+@end
+
+for i in range(0, 5, 2):
+    continue
+@end
+
+for key in dict.keys():
+    print(dict[key])
+@end

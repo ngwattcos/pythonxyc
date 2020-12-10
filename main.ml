10d58f33bbdc195a6743109ffa838819da82ebf2
Scott Wang <curiosity142000@gmail.com>
2020-12-10 18:32:15 -0500

    compiler enhancements

--- main.ml
+++ main.ml
@@ -11,23 +11,25 @@ let banner = "------------------------------------------------------------------
 
 let l = Array.length Sys.argv - 1
 let path = Sys.argv.(1)
-let inDir = Sys.argv.(2)
-let outDir = Sys.argv.(3)
-let opt = if l > 4 then Sys.argv.(4) else "t"
+let inDir = if l >= 2 then Sys.argv.(2) else ""
+let outDir = if l >= 3 then Sys.argv.(3) else ""
+let opt = if l >= 4 then Sys.argv.(4) else if l >= 2 then Sys.argv.(2) else "T"
 
 let flag = match opt with
-| "l" | "L" | "p" | "P" | "a" | "A" | "g" | "G" | "t" | "T" -> opt 
+| "l" | "L" | "p" | "P" | "a" | "A" | "g" | "G" | "t" | "T" -> opt
 | _ -> "t"
 
 let input_path = "../" ^ inDir ^ path
 let output_path = "../" ^ outDir ^ path
 
-let filename = match flag with 
+let test_file = match flag with 
 | "l" | "L" -> "tests/lexer/lex" ^ path ^ ".pyx"
 | "p" | "P" | "a" | "A" | "g" | "G" -> "tests/parser/parse" ^ path ^ ".pyx"
 | "t" | "T" | _ -> "tests/translator/translate" ^ path ^ ".pyx"
 
-let lexbuf = match Lexing.from_channel (open_in input_path)
+let lexbuf = match flag with
+| "t" -> Lexing.from_channel (open_in input_path)
+| "T" | _ -> Lexing.from_channel (open_in test_file)
 
 (* enable print lexing *)
 let token = match flag with 
@@ -51,7 +53,7 @@ let write_output = output_string (open_out output_path)
 
 
 (* print tranformed AST *)
-let _ = 
+let _ =
   match flag with
   | "T" -> print_string banner; ast |> Transform.translate |> Buffer.contents |> print_endline
   | "t" -> ast |> Transform.translate |> Buffer.contents |> write_output

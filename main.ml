2285f23b1e02df61c3c2d5bb7a005567b9ee5834
Scott Wang <curiosity142000@gmail.com>
2020-12-10 18:06:23 -0500

    support writing to file

--- main.ml
+++ main.ml
@@ -1,3 +1,4 @@
+
 open Ast
 open Lexer
 open Lexer_verbose
@@ -9,24 +10,29 @@ open Print
 let banner = "----------------------------------------------------------------------------\n"
 
 let l = Array.length Sys.argv - 1
-let num = Sys.argv.(1)
-let opt = if 2 <= l then Sys.argv.(2) else "t"
+let path = Sys.argv.(1)
+let inDir = Sys.argv.(2)
+let outDir = Sys.argv.(3)
+let opt = if l > 4 then Sys.argv.(4) else "t"
 
 let flag = match opt with
 | "l" | "L" | "p" | "P" | "a" | "A" | "g" | "G" | "t" | "T" -> opt 
 | _ -> "t"
 
-let filename = match flag with 
-| "l" | "L" -> "tests/lexer/lex" ^ num ^ ".pyx"
-| "p" | "P" | "a" | "A" | "g" | "G" -> "tests/parser/parse" ^ num ^ ".pyx"
-| "t" | "T" | _ -> "tests/translator/translate" ^ num ^ ".pyx"
+let input_path = "../" ^ inDir ^ path
+let output_path = "../" ^ outDir ^ path
 
-let lexbuf = Lexing.from_channel (open_in filename)
+let filename = match flag with 
+| "l" | "L" -> "tests/lexer/lex" ^ path ^ ".pyx"
+| "p" | "P" | "a" | "A" | "g" | "G" -> "tests/parser/parse" ^ path ^ ".pyx"
+| "t" | "T" | _ -> "tests/translator/translate" ^ path ^ ".pyx"
+
+let lexbuf = match Lexing.from_channel (open_in input_path)
 
 (* enable print lexing *)
 let token = match flag with 
 | "l" | "L" -> Lexer_verbose.token
-| _ -> Lexer.token 
+| _ -> Lexer.token
 
 let ast =
   try Grammar.program token lexbuf
@@ -41,8 +47,12 @@ let _ = match flag with
   | "p" | "P" | "a" | "A" | "g" | "G" -> print_string banner; print_verbose ast
   | _ -> ()
 
+let write_output = output_string (open_out output_path)
+
+
 (* print tranformed AST *)
 let _ = 
   match flag with
-  | "t" | "T" -> print_string banner; ast |> Transform.translate |> Buffer.contents |> print_endline
+  | "T" -> print_string banner; ast |> Transform.translate |> Buffer.contents |> print_endline
+  | "t" -> ast |> Transform.translate |> Buffer.contents |> write_output
   | _ -> ()
